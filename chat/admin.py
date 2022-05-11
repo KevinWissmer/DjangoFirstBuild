be0979b42726dba69c96fd9ed285bc17b2c65741
KevinWissmer <kevin.wissmer@mail.de>
2022-05-11 16:10:42 +0200

    admin panel extended

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from chat.models import Message
 
-# Register your models here.
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'author', 'receiver')
+    list_display = ('author', 'text', 'receiver')
+    list_filter = ('created_at',)
+    search_fields = ('text','author',)
+
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+admin.site.register(Message, MessageAdmin)
+
+
+
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.fields import DateField
+from django.conf import settings
+from datetime import date
 
-# Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = DateField(default=date.today)
+    #chat = chatklasse link
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recevier_message_set')
