4776aa710a5f95f72ebddc9dbf4ebda28c5fd3d7
KevinWissmer <kevin.wissmer@mail.de>
2022-05-12 19:28:28 +0200

    login requested

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Message, Chat
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login/')
 def index(request):
