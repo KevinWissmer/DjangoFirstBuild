39d2b36ce057ae56e14fb5680d86375ffd8df7d1
KevinWissmer <kevin.wissmer@mail.de>
2022-05-12 15:36:31 +0200

    input post added in views and index chat

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        print("recieved data: " + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'name': request})
